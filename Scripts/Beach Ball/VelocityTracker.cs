f518214872a2ac9b9804acd48986f7b51aeccae6
HuskyGT <103238785+HuskyGT@users.noreply.github.com>
2022-08-21 00:57:51 -0400

    Add files via upload

---
+++ Scripts/Beach Ball/VelocityTracker.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace ObjectVelocityTracker
+{
+    public class VelocityTracker : MonoBehaviour
+    {
+        public float force;
+        public Vector3 velocity;
+        public Vector3 LastPos;
+
+        void Start()
+        {
+            LastPos = Vector3.zero;
+            velocity = transform.position;
+        }
+        void Update()
+        {
+            velocity = transform.position - LastPos / Time.deltaTime / 5;
+            //velocity = transform.position - LastPos / Time.deltaTime / 15;
+            LastPos = transform.position;
+            force = velocity.magnitude;
+        }
+    }
+}
+    
+
