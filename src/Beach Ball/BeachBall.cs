5eff67107f510d3478d1625aefde9d02a8fe3fd8
HuskyGT <103238785+HuskyGT@users.noreply.github.com>
2022-08-21 01:01:27 -0400

    Add files via upload

---
+++ src/Beach Ball/BeachBall.cs
@@ -0,0 +1,37 @@
+﻿using ObjectVelocityTracker;
+using UnityEngine;
+public class BeachBallStuff : MonoBehaviour
+{
+    void Start()
+    {
+        GameObject.Find("LeftHandTriggerCollider").AddComponent<VelocityTracker>();
+        GameObject.Find("RightHandTriggerCollider").AddComponent<VelocityTracker>();
+        gameObject.transform.SetParent(null, true);
+    }
+    void Update()
+    {
+        gameObject.GetComponent<Rigidbody>().AddForce(0f, -2f, 0f);
+    }
+    void OnTriggerEnter(Collider col)
+    {
+            if (col.gameObject.GetComponent<VelocityTracker>() != null)
+            {
+                if (!gameObject.GetComponent<AudioSource>().isPlaying)
+                {
+                    if (col.name == "LeftHandTriggerCollider")
+                    {
+                        GorillaTagger.Instance.StartVibration(true, 0.3f, 0.1f);
+                    }
+                    if (col.name == "RightHandTriggerCollider")
+                    {
+                        GorillaTagger.Instance.StartVibration(false, 0.3f, 0.1f);
+                    }
+                        gameObject.GetComponent<AudioSource>().PlayOneShot(gameObject.GetComponent<AudioSource>().clip);
+                }
+                print($"{col.gameObject.name} Hit Beach Ball with: {col.gameObject.GetComponent<VelocityTracker>().velocity} velocity");
+                gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
+                gameObject.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+                gameObject.GetComponent<Rigidbody>().AddExplosionForce(col.gameObject.GetComponent<VelocityTracker>().force, col.ClosestPoint(transform.position), 5);
+            }
+    }
+}
