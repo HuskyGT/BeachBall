6000a7ed0ab06942de16f26f232214331785db51
HuskyGT <103238785+HuskyGT@users.noreply.github.com>
2022-08-28 17:26:15 -0400

    Update PluginInfo.cs

--- src/PluginInfo.cs
+++ src/PluginInfo.cs
@@ -1,12 +1,9 @@
-﻿namespace BeachBall
+namespace BeachBall
 {
-	/// <summary>
-	/// This class is used to provide information about your mod to BepInEx.
-	/// </summary>
 	class PluginInfo
 	{
 		public const string GUID = "Husky.JamSubmission.BeachBall";
 		public const string Name = "BeachBall";
-		public const string Version = "1.0.0";
+		public const string Version = "1.0.1";
 	}
 }
