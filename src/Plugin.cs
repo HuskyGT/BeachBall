c4e1738dc2e654a59a56e4886de910a785f020c1
HuskyGT <103238785+HuskyGT@users.noreply.github.com>
2022-08-28 17:24:34 -0400

    Update Plugin.cs

--- src/Plugin.cs
+++ src/Plugin.cs
@@ -1,8 +1,7 @@
-﻿using System;
+using System;
 using System.IO;
 using System.Reflection;
 using BepInEx;
-using ObjectVelocityTracker;
 using UnityEngine;
 using UnityEngine.XR;
 using Utilla;
@@ -14,40 +13,35 @@ namespace BeachBall
 	[BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
 	public class BeachBallMain : BaseUnityPlugin
 	{
-		bool Can = false;
 		public static BeachBallMain instance;
+		public bool Can = false;
 		GameObject RightHand;
 		GameObject LeftHand;
-		public GameObject Cabinetobj;
-		public GameObject BB;
-		public GameObject HandTrigger;
+		GameObject Cabinetobj;
+		public static GameObject BB;
 		void OnEnable()
 		{
 
 			HarmonyPatches.ApplyHarmonyPatches();
 			Utilla.Events.GameInitialized += OnGameInitialized;
 		}
-
 		void OnDisable()
 		{
 			HarmonyPatches.RemoveHarmonyPatches();
 			Utilla.Events.GameInitialized -= OnGameInitialized;
 		}
-		// unneeded maybe Cabinet.transform.rotation = new Quaternion(0f, 161f, 0f, 0f);
 		void OnGameInitialized(object sender, EventArgs e)
 		{
 			Stream str = Assembly.GetExecutingAssembly().GetManifestResourceStream("Jam_Submission.Assets.beachballers");
 			AssetBundle bundle = AssetBundle.LoadFromStream(str);
 			GameObject beachballcabinet = bundle.LoadAsset<GameObject>("Ball Cabinet");
 			Cabinetobj = Instantiate(beachballcabinet);
-			//Cabinetobj.transform.localScale = new Vector3(0.3f, 0.3f, 0.3f);
-			//Cabinetobj.transform.position = new Vector3(-68.5422f, 11.9302f, -84.247f);
 			Cabinetobj.transform.Find("Cabinet/LeftDoorOpen/t").gameObject.AddComponent<DoorTriggers>();
 			Cabinetobj.transform.Find("Cabinet/RightDoorOpen/t").gameObject.AddComponent<DoorTriggers>();
 			Cabinetobj.transform.Find("Cabinet/LeftDoorClose/t").gameObject.AddComponent<DoorTriggers>();
 			Cabinetobj.transform.Find("Cabinet/RightDoorClose/t").gameObject.AddComponent<DoorTriggers>();
 			Cabinetobj.transform.Find("Cabinet/Reset Button").gameObject.AddComponent<ResetTrigger>();
-			BB = Cabinetobj.transform.Find("Cabinet/Beach Ball").gameObject;
+			BB = Cabinetobj.transform.Find("Cabinet/Beach BallMonoObject").gameObject;
 			BB.AddComponent<BeachBallStuff>();
 			LeftHand = GameObject.Find("OfflineVRRig/Actual Gorilla/rig/body/shoulder.L/upper_arm.L/forearm.L/hand.L/palm.01.L");
 			RightHand = GameObject.Find("OfflineVRRig/Actual Gorilla/rig/body/shoulder.R/upper_arm.R/forearm.R/hand.R/palm.01.R");
@@ -56,40 +50,49 @@ namespace BeachBall
         {
 			if (BB != null)
             {
-				bool Leftgrip;
-				InputDevices.GetDeviceAtXRNode(XRNode.LeftHand).TryGetFeatureValue(CommonUsages.gripButton, out Leftgrip);
-				bool Rightgrip;
-				InputDevices.GetDeviceAtXRNode(XRNode.RightHand).TryGetFeatureValue(CommonUsages.gripButton, out Rightgrip);
-				if (Rightgrip && !Leftgrip)
+				bool Lefttrigger;
+				InputDevices.GetDeviceAtXRNode(XRNode.LeftHand).TryGetFeatureValue(CommonUsages.triggerButton, out Lefttrigger);
+				bool Righttrigger;
+				InputDevices.GetDeviceAtXRNode(XRNode.RightHand).TryGetFeatureValue(CommonUsages.triggerButton, out Righttrigger);
+				if (Righttrigger && !Lefttrigger)
 				{
-					if (Vector3.Distance(RightHand.transform.position, BB.transform.position) < 3)
+					if (Vector3.Distance(RightHand.transform.position, BB.transform.position) < 1)
 					{
+						if (!Can)
+						{
+							BB.GetComponent<AudioSource>().enabled = false;
+						}
 						Can = true;
 						BB.GetComponent<Rigidbody>().velocity = Vector3.zero;
 						BB.transform.SetParent(RightHand.transform, true);
-						BB.transform.localPosition = new Vector3(-0.3f, 0f, 0f);
+						BB.transform.localPosition = new Vector3(-0.17f, 0f, 0f);
 					}
 				}
 				else if (Can)
 				{
 					Can = false;
+					BB.GetComponent<AudioSource>().enabled = true;
 					BB.transform.SetParent(null, true);
 				}
-
-
-				if (Leftgrip && !Rightgrip)
+			
+				if (Lefttrigger)
 				{
-					if (Vector3.Distance(LeftHand.transform.position, BB.transform.position) < 3)
+					if (Vector3.Distance(LeftHand.transform.position, BB.transform.position) < 1)
 					{
+						if (!Can)
+						{
+							BB.GetComponent<AudioSource>().enabled = false;
+						}
 						Can = true;
 						BB.GetComponent<Rigidbody>().velocity = Vector3.zero;
 						BB.transform.SetParent(LeftHand.transform, true);
-						BB.transform.localPosition = new Vector3(0f, 0f, 0.3f);
+						BB.transform.localPosition = new Vector3(0f, 0f, 0.17f);
 					}
 				}
 				else if (Can)
 				{
 					Can = false;
+					BB.GetComponent<AudioSource>().enabled = true;
 					BB.transform.SetParent(null, true);
 				}
 			}
